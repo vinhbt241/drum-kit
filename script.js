83fbe13fedbdd1794e6a0e8d50f410a6989068bf
Bui The Vinh <vinhbuith32412000@gmail.com>
2022-01-12 15:44:08 +0700

    Add button transition animation

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
 const keyInput = document.querySelector("#key-input");
+const btns = document.querySelectorAll("button");
 
 function playSound(e) {
-    const audio = document.querySelector(`#key-${e.keyCode}`);
+    const audio = document.querySelector(`audio[id="key-${e.keyCode}"]`);
     if (!audio) return;
     audio.currentTime = 0;
     audio.play();
 }
 
-keyInput.addEventListener("keydown", playSound)
+function addTrans(e) {
+    const btn = document.querySelector(`button[id="key-${e.keyCode}"]`);
+    btn.classList.add('trans');
+}
+
+function removeTrans(e) {
+    if (e.propertyName !== "transform") return
+    this.classList.remove("trans");
+}
+
+keyInput.addEventListener("keydown", playSound);
+keyInput.addEventListener("keydown", addTrans);
+
+btns.forEach(btn => btn.addEventListener("transitionend", removeTrans));
